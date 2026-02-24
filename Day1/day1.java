a6991ae82e212a831b387d327110ae42b83dd4ab
Prerna-Nagpal <nagpalprerna4@gmail.com>
2026-02-24 10:45:06 +0530

    single-line,multi-line

--- Day1/day1.java
+++ Day1/day1.java
@@ -1,3 +1,5 @@
+//Day1 of 100daysofJava challenge-signle line comments
+/*we will ace this race -multi line comments */
 public class day1 {
     public static void main(String[] args){
         System.out.println("Hello World!");
