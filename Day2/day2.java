83082177a64b76fa08c33fc424d045bac257a81b
Prerna-Nagpal <nagpalprerna4@gmail.com>
2026-03-03 16:56:14 +0530

    day2 of java

---
+++ Day2/day2.java
@@ -0,0 +1,11 @@
+//Day2 - Variables and Data Types
+public class day2 {
+    public static void main(String[] args){
+    int age = 22;
+    double height = 5.9;
+    String name = "Prerna";
+    System.out.println("Age:"+age);
+    System.out.println("Height:"+height);
+    System.out.println("Name:"+name);
+}
+}
