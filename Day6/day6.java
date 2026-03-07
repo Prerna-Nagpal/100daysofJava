8ed14971c470a278046b28a1651be1808c104a2e
Prerna-Nagpal <nagpalprerna4@gmail.com>
2026-03-07 17:43:24 +0530

    adding task of day6

---
+++ Day6/day6.java
@@ -0,0 +1,21 @@
+
+public class day6 {
+    public static void main(String[] args) {
+        String n = "25";
+        int num = Integer.parseInt(n);
+        System.out.println(num);
+
+        String n1 = "19.99";
+        float num1 = Float.parseFloat(n1);
+        System.out.println(num1);
+
+        int number= 100;
+        String str = String.valueOf(number);
+        System.out.println(str);
+        String str2 = Integer.toString(number);
+        System.out.println(str2);
+
+     
+        
+    }
+}
