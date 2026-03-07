25c544a5933ab5e3d0eec10605f8b8cba605f8d5
Prerna-Nagpal <nagpalprerna4@gmail.com>
2026-03-07 17:26:29 +0530

    adding task of day5

---
+++ Day5/task1.java
@@ -0,0 +1,11 @@
+// Increment and Decrement Operators in Java
+public class task1 {
+    public static void main(String[] args) {
+        int num =10;
+        num++;
+        System.out.println("After increment: " + num);
+        num--;
+        System.out.println("After decrement: " + num);
+
+    }
+}
---
+++ Day5/task2.java
@@ -0,0 +1,16 @@
+// use of compund assignment operators in Java
+public class task2 {
+    public static void main(String[] args) {
+        int num =10;
+        num+=5;
+        System.out.println("After adding 5: " + num);
+        num-=3;
+        System.out.println("After subtracting 3: " + num);
+        num*=2;
+        System.out.println("After multiplying by 2: " + num);
+        num/=4;
+        System.out.println("After dividing by 4: " + num);
+        num%=3;
+        System.out.println("After modulus by 3: " + num);
+    }
+}
